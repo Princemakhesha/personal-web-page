5191f651724f7fecaeb5574c65d5509de0bd1034
unknown <princemakhesha@gmail.com>
2023-05-28 14:37:38 +0200

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,47 @@
+import handlebars from 'vite-plugin-handlebars';
+
+// import { defineConfig } from 'vite';
+
+
+import data from "./data.json" assert {type:'json'};
+
+export default {
+    // base: '/responsive_webpages/',
+    plugins: [handlebars({
+      context : {
+          data
+      }
+    })],
+  };
+
+// export default defineConfig({
+//   // config attributes goes here
+// });
+
+// import handlebars from 'vite-plugin-handlebars';
+
+// import data from './data.json' assert {type:'json'};
+
+// const basicContext = {
+//         title : "Thizwilondi Prince Makhesha",
+//         show : false,
+//         photo: "image1.jpeg",
+//         fruits : [
+//             "apples",
+//             "oranges",
+//             "bananas"
+//         ],
+//         isClient : true,
+//         fruitSales : [
+//             {day : "Monday", apples : 13, oranges : 34},
+//             {day : "Tuesday", apples : 17, oranges : 28},
+//             {day : "Thursday", apples : 7, oranges : 45},
+//         ]
+//     };
+
+// export default {
+//   plugins: [handlebars({
+//     context : basicContext
+//   })]
+// };
+
